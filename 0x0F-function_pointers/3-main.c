7a32fdc7dccaeda3115cc1ce19bda4e2bfce4ec5
devmike93 <msk.megbenu@gmail.com>
2023-10-12 00:47:28 +0200

    fixed 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[2][1])
+	if ((argv[2][0] == '/' || argv[2][0] == '%') || argv[2][1])
 	{
 		printf("Error\n");
 		exit(100);
