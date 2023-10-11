f09d2ca6fee879aa660b55098011de4826b1d42c
devmike93 <msk.megbenu@gmail.com>
2023-10-12 00:45:11 +0200

    fixed 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,10 +18,10 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1])
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && argv[2][1])
 	{
 		printf("Error\n");
-		exit(99);
+		exit(100);
 	}
 
 	operation = get_op_func(argv[2]);
