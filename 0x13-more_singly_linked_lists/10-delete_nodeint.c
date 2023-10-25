b8afc265085c95f67fc97f7c4149655e886e0e02
devmike93 <msk.megbenu@gmail.com>
2023-10-25 03:24:13 +0200

    redid 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,45 +1,43 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index
- * of a linked list.
- * @head: head of a list.
- * @index: index of the list where the node is
- * deleted.
+ * delete_nodeint_at_index - dels node of LL
+ * @head: head of a LL.
+ * @index: index of deleted node
  * Return: 1 if it succeeded, -1 if it failed.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int k;
-	listint_t *prev_node;
-	listint_t *next_node;
+	listint_t *prev_h;
+	listint_t *del_node;
 
-	prev_node = *head;
+	prev_h = *head;
 
 	if (index != 0)
 	{
-		for (k = 0; k < index - 1 && prev_node != NULL; k++)
+		for (k = 0; k < index - 1 && prev_h != NULL; k++)
 		{
-			prev_node = prev_node->next_node;
+			prev_h = prev_h->del_node;
 		}
 	}
 
-	if (prev_node == NULL || (prev_node->next_node == NULL && index != 0))
+	if (prev_h == NULL || (prev_h->del_node == NULL && index != 0))
 	{
 		return (-1);
 	}
 
-	next_node = prev_node->next_node;
+	del_node = prev_h->del_node;
 
 	if (index != 0)
 	{
-		prev_node->next_node = next_node->next_node;
-		free(next_node);
+		prev_h->del_node = del_node->del_node;
+		free(del_node);
 	}
 	else
 	{
-		free(prev_node);
-		*head = next_node;
+		free(prev_h);
+		*head = del_node;
 	}
 
 	return (1);
