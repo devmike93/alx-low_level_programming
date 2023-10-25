a57e56293818549dff101af13d48ddc806e8d179
devmike93 <msk.megbenu@gmail.com>
2023-10-25 04:38:35 +0200

    im tired af

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,36 +1,74 @@
 #include "lists.h"
-
-
 /**
- * free_listint_safe - fxn to free a linked list
- * @h: first node of list(head).
- * Return: nodesize that was freed.
+ * free_listp2 - helperfxn that frees stuff
+ * @head: first node of a list.
+ * Return: no return.
  */
+void free_listp2(listp_t **head)
+{
+	listp_t *ptr_temp;
+	listp_t *ptr_curr;
 
+	if (head != NULL)
+	{
+		ptr_curr = *head;
+		while ((ptr_temp = ptr_curr) != NULL)
+		{
+			ptr_curr = ptr_curr->next;
+			free(ptr_temp);
+		}
+		*head = NULL;
+	}
+}
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: first node of a list.
+ * Return: the size of the list that was freed.
+ */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t nodesize = 0;
-	listint_t *ptr_cur;
-	listint_t *stack = NULL;
+	size_t freed_nodes = 0;
+	listp_t *stack_top = NULL;
+	listint_t *current_node;
+	listp_t *temp;
 
 	while (*h != NULL)
 	{
-		ptr_cur = *h;
+		listp_t *new_node = malloc(sizeof(listp_t));
+
+		if (new_node == NULL)
+			exit(98);
+
+		new_node->p = (void *)*h;
+		new_node->next = stack_top;
+		stack_top = new_node;
+
+		temp = stack_top;
+
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+			if (*h == temp->p)
+			{
+				*h = NULL;
+				while (stack_top != temp)
+				{
+					listp_t *to_free = stack_top;
+
+					stack_top = stack_top->next;
+
+					free(to_free);
+				}
+				free_listp2(&stack_top);
+				return (freed_nodes);
+			}
+		}
+		current_node = *h;
 		*h = (*h)->next;
-
-		ptr_cur->next = stack;
-		stack = ptr_cur;
+		free(current_node);
+		freed_nodes++;
 	}
-
-	while (stack != NULL)
-	{
-		ptr_cur = stack;
-		stack = stack->next;
-
-		free(ptr_cur);
-		nodesize++;
-	}
-
 	*h = NULL;
-	return (nodesize);
+	free_listp2(&stack_top);
+	return (freed_nodes);
 }
