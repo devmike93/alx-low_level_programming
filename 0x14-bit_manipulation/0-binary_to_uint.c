b76b6acc42da215c0f8a468c8492019df293bd1a
devmike93 <msk.megbenu@gmail.com>
2023-10-27 04:37:37 +0200

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,11 +10,12 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int uns = 0;
+	int k;
 
 	if (b == NULL)
 		return (0);
 
-	for (int k = 0; b[k] != '\0'; k++)
+	for (k = 0; b[k] != '\0'; k++)
 	{
 		if (b[k] != '0' && b[k] != '1')
 			return (0);
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,9 +8,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	if (n)
 	{
-		print_binary(n / 2);
+		if (n > 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	_putchar((n % 2) + '0');
+	else
+	{
+		_putchar('0');
+	}
 }
