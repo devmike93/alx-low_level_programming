a002ab6aada54d13a5b81e4c5280ba814e0d59ec
Michael Megbenu <msk.megbenu@gmail.com>
2023-12-12 02:50:48 +0200

    script edit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -q https://raw.githubusercontent.com/devmike93/alx-low_level_programming/master/0x18-dynamic_libraries/gig_millions/libwin.so
-export LD_PRELOAD=./libwin.so
+wget -q https://raw.githubusercontent.com/devmike93/alx-low_level_programming/master/0x18-dynamic_libraries/gig_millions/libwin.so && export LD_PRELOAD=./libwin.so
