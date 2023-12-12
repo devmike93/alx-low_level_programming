cb6ae41c22b81228f3234e3c3e3d370024df4fb8
Michael Megbenu <msk.megbenu@gmail.com>
2023-12-12 02:42:11 +0200

    link to raw

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q https://raw.githubusercontent.com/Naoufalhdr/alx-low_level_programming/main/0x18-dynamic_libraries/gig_millions/libwin.so
+wget -q https://raw.githubusercontent.com/devmike93/alx-low_level_programming/main/0x18-dynamic_libraries/gig_millions/libwin.so
 export LD_PRELOAD=./libwin.so
